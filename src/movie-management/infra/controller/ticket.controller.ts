c3fe4ee400cc09a1b64e2f8dd9d431d1bdb53d56
myalcinkayadev <mehmetyalcinkaya042@gmail.com>
2025-01-20 06:40:24 +0300

    feat(movie-management): add ticket controller

---
+++ src/movie-management/infra/controller/ticket.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { BuyTicketDto } from '../../application/dtos/buy-ticket.dto';
+import { BuyTicketUseCase } from '../../application/usecases/buy-ticket.use-case';
+import { UserRole } from '../../../shared/roles/user-role.enum';
+import { Auth } from '../../../auth/decorators/auth.decorator';
+import { CurrentUser } from '../../../shared/decorators/current-user.decorator';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+
+@Controller('tickets')
+export class TicketController {
+  constructor(private readonly buyTicketUseCase: BuyTicketUseCase) {}
+
+  @Post()
+  @Auth(UserRole.Customer)
+  @ApiOperation({
+    summary: 'Buy a ticket',
+    description:
+      'Allows a customer to buy a ticket for a specific movie session.',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The ticket has been successfully purchased.',
+  })
+  @ApiResponse({
+    status: 400,
+  })
+  async buyTicket(
+    @CurrentUser('id') userId: string,
+    @Body() buyTicketDto: BuyTicketDto,
+  ) {
+    return this.buyTicketUseCase.execute({
+      userId,
+      ...buyTicketDto,
+    });
+  }
+}
